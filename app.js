5d80c07e9cc1452eee0d652d487b64b87bf83634
Anzal Beg <anzal.b@hcl.com>
2016-07-15 09:16:07 +0000

    chat app

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
-/*eslint-env node*/
-
+/*eslint-env node, express*/
 //------------------------------------------------------------------------------
 // node.js starter application for Bluemix
 //------------------------------------------------------------------------------
@@ -7,16 +6,35 @@
 // This application uses express as its web server
 // for more info, see: http://expressjs.com
 var express = require('express');
+var path = require('path');
+var bodyParser = require('body-parser');
+var routes = require('./routes/index');
 
 // cfenv provides access to your Cloud Foundry environment
 // for more info, see: https://www.npmjs.com/package/cfenv
 var cfenv = require('cfenv');
 
+
 // create a new express server
 var app = express();
+var request = require('request');
+
+// create a new viewing engine
+app.set('views', path.join(__dirname, 'views'));
+
+// create a new Temaplting engine
+app.engine('html', require('ejs').renderFile);
 
 // serve the files out of ./public as our main files
 app.use(express.static(__dirname + '/public'));
+app.set('view engine','html');
+app.use('/', routes);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+
+
 
 // get the app environment from Cloud Foundry
 var appEnv = cfenv.getAppEnv();
